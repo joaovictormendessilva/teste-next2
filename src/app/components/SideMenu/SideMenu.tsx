573d1043d40ca158c5d839a3af47644167db9b1d
João Victor <joao_victor-08@hotmail.com>
2023-08-14 17:28:55 -0300

    Teste Drawer Sticky

--- src/app/components/SideMenu/SideMenu.tsx
+++ src/app/components/SideMenu/SideMenu.tsx
@@ -193,7 +193,7 @@ export function SideMenu({ children }: ISideMenuProps) {
       </AppBar>
       {/* End - Appbar superior */}
 
-      <Drawer variant="permanent" PaperProps={{ sx: { position: "inherit", width: openDrawer ? "145px" : "65px", transition: "width 0.3s ease-in-out", backgroundColor: theme.palette.primary.main, overflow: "hidden" } }}>
+      <Drawer variant="permanent" PaperProps={{ sx: { position: "sticky", width: openDrawer ? "145px" : "65px", transition: "width 0.3s ease-in-out", backgroundColor: theme.palette.primary.main, overflow: "hidden" } }}>
         {/* Botão que alterna o tamanho do drawer */}
         <List>
           <ListItem disablePadding sx={{ display: 'block', marginTop: 8 }}>
