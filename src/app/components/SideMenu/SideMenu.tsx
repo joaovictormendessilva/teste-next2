47adc65afd124ebd296ce09bcff5aa12ef9d8f32
João Victor <joao_victor-08@hotmail.com>
2023-08-15 08:52:46 -0300

    drawer menu fixed

--- src/app/components/SideMenu/SideMenu.tsx
+++ src/app/components/SideMenu/SideMenu.tsx
@@ -215,7 +215,7 @@ export function SideMenu({ children }: ISideMenuProps) {
         {/* End - Botão que alterna o tamanho do drawer */}
 
         {/* Menus do Drawer */}
-        <List sx={{ position: "sticky", top: "100px" }}>
+        <List sx={{ position: "fixed", top: "100px" }}>
           <ListItem disablePadding sx={{ display: 'block' }}>
             <ListItemButton
               // href="https://google.com"
